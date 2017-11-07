8b02e06b4648aa18c03f70d807a39d609276a8e1
hkpogo <hendrik.kegel@mni.thm.de>
2017-11-07 11:49:17 +0100

    work in progress

--- main.c
+++ main.c
@@ -1,4 +1,8 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <time.h>
 #include "refs.h"
+
 
 string readInput(void) {
     char * line = malloc(100), * linep = line;
@@ -32,15 +36,39 @@ string readInput(void) {
     return linep;
 }
 
+int getChildStatus (int status) {
+    if (WIFEXITED (status)) {
+        //printf ("Kind normal beendet mit Rückgabewert %d\n", WEXITSTATUS(status));
+        return 0;
+    }
+    else if (WIFSIGNALED (status)) {
+        /* printf ("Kind mit Signalnummer %d beendet\n", WTERMSIG(status));
+        /* WCOREDUMP könnte hier stehen */
+        return WTERMSIG(status);
+    }
+    else if (WIFSTOPPED (status)) {
+        /*printf ("Kind wurde angehalten mit Signalnummer %d\n",
+                WSTOPSIG(status));
+        /* I.d.R. wird dies SIGSTOP sein, aber es gibt */
+        /* ja auch noch das ptrace()-Interface.        */
+        return -1;
+    }
+}
+
 int main() {
 
     // vars
     struct Program program;
     string input;
+    int pId;
     int i, j;
+    int sum_of_usertime;
 
     //main routine
     while(true){
+
+        printf("> ");
+        sum_of_usertime = 0;
 
         // init/reset program
         program.size = 0;
@@ -55,7 +83,7 @@ int main() {
         //read input
         input = readInput();
         if (strlen(input) > 500) perror("IndexOutOfBound: input too long");
-        if (input[0] == '>') break;
+        if (input == NULL) exit(0); //todo
 
         //put input into peaces
         string part = strtok(input, ";");
@@ -68,8 +96,9 @@ int main() {
         for (i=0; i < program.size; i++){
             string tmp = strdup(program.command[i].toString);
             program.command[i].progName = strtok(tmp, " ");
+            program.command[i].args[0] = program.command[i].progName;
             part = strtok(NULL, " ");
-            j=0;
+            j=1;
             while(part != NULL){
                 program.command[i].args[j] = part;
                 part = strtok(NULL, " ");
@@ -77,8 +106,101 @@ int main() {
             }
         }
 
+        struct ChildProcess child[program.size];
+
+        //fork
+        for (i = 0; i < program.size; i++) {
+            if ((child[i].pid=fork()) == 0) {
+
+                execvp(program.command[i].progName, program.command[i].args);
+
+                perror("Execvp error");
+                _exit(1);
+            }
+            if (child[i].pid < 0) {
+                perror("Fork error");
+            }
+        }
+
+        //wait for execution
+        for (i = 0; i < program.size; i++) {
+            if (child[i].pid > 0) {
+                int status;
+                child[i].startTime = clock();
+                waitpid(child[i].pid, &status, 0);
+                child[i].endTime = clock();
+                child[i].status = getChildStatus(status);
+                if (status > 0) { //error
+                    child[i].exitedWithError = true;
+                }
+            } else {
+                //process did never started
+                child[i].exitedWithError = true;
+            }
+        }
+
+        /*
+        for (i = 0; i < program.size; i++) {
+            printf("Command: %d: %s \n", i, program.command[i].toString);
+            printf("pid: %d \n", child[i].pid);
+            printf("status: %d \n", child[i].status);
+            printf("startTime: %d \n", child[i].startTime);
+            printf("endTime: %d \n", child[i].endTime);
+            printf("elipsed time: %d \n", child[i].endTime - child[i].startTime);
+            printf("failed: %d \n\n", child[i].exitedWithError);
+        }
+         */
+
+        //print
+        for (i = 0; i < program.size; i++) {
+            if (child[i].exitedWithError != 0) {
+                printf("%s: [execution error]");
+                continue;
+            }
+            printf("%s: user time = %d \n", program.command[i].progName, child[i].endTime - child[i].startTime);
+        }
+
+        //end test
 
 
+
+        /*
+        for(i=0; i < program.size; i++){
+            int childStatus;
+            clock_t before, difference;
+
+            pId = fork(); /* Hier wird eine Kopie des laufenden Prozesses erzeugt!
+            if (pId < 0) {
+                perror("fork failed");
+                exit(EXIT_FAILURE);
+            }
+            else if (pId == 0){
+                /* child process
+
+                childStatus = execvp(program.command[i].progName, program.command[i].args);
+                perror(1);
+            }
+            else {
+                /* Vaterprozess; n = Prozess-ID des Kindes
+                before = clock();
+                pId = wait(&childStatus);
+                difference = clock() - before;
+
+                switch(getChildStatus(childStatus))
+                {
+                    case 0:
+                        printf("%s: user time = %d \n", program.command[i].progName, difference);
+                        sum_of_usertime += difference;
+                        break;
+                    default:
+                        printf("%s: %s\n", program.command[i].progName, "[execution error]");
+                }
+
+                /* In "&status" erscheint der mit "return ...;" aus main() zurückgegebene Wert!
+            }
+        }
+
+        printf("sum of user times = %d \n", sum_of_usertime); */
         printf("run completed.\n");
     }
 
--- main.h
+++ main.h
@@ -8,12 +8,19 @@ struct Command{
     string toString;
     string progName;
     string args[MAX_ARGS];
-}command;
+};
 
 struct Program{
     struct Command command[MAX_COMMAND];
     int size;
-}program;
+};
 
+struct ChildProcess{
+    clock_t startTime;
+    clock_t endTime;
+    pid_t pid;
+    int status;
+    bool exitedWithError;
+};
 
 string readInput(void);
